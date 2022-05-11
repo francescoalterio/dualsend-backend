a1cc46f3aeea8e8031a5016179def2a15577134a
francescoalterio <francescoalteriog@gmail.com>
2022-05-10 20:34:08 -0400

    add create dir and get file

--- utils/getContentDir.js
+++ utils/getContentDir.js
@@ -1,31 +1,30 @@
-const fs = require('fs');
-const getLocalIP = require('./getLocalIP.js');
+const fs = require("fs");
+const getLocalIP = require("./getLocalIP.js");
 
 const getContentDir = async (path) => {
-   try {
-    const contentDir = await fs.promises.opendir(path)
+  try {
+    const contentDir = await fs.promises.opendir(path);
     const myIP = await getLocalIP();
     const data = {
-        myIP,
-        path,
-        content: {
-            directories: [],
-            files: []
-        } 
-    }
+      myIP,
+      path,
+      content: {
+        directories: [],
+        files: [],
+      },
+    };
     for await (const dirent of contentDir) {
-        if(dirent.isDirectory()) {
-            data.content.directories.push(dirent.name)
-        } else {
-            data.content.files.push(dirent.name)
-        }
-
+      if (dirent.isDirectory()) {
+        data.content.directories.push(dirent.name);
+      } else {
+        data.content.files.push(dirent.name);
+      }
     }
 
-    return data
-   } catch (error) {
-    return 'Error: ' + error
-   }
-}
+    return data;
+  } catch (error) {
+    return "Error: " + error;
+  }
+};
 
-module.exports = getContentDir
+module.exports = getContentDir;
